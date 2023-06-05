49d658cebf37812d31e946ba48d80edb41714509
StdParrot-OS <112552484+StdParrot-OS@users.noreply.github.com>
2023-06-05 18:14:52 +0530

    Update config.py
    Signed-off-by: StdParrot-OS <112552484+StdParrot-OS@users.noreply.github.com>

--- config.py
+++ config.py
@@ -21,12 +21,12 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "1356469075").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/AnonymousX1025/AnonXMusic")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/StdParrot-OS/AnonXMusic-1")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/DevilsHeavenMF")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ll_MY_ABOUT_ll")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/girlfriend_4_you")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 
